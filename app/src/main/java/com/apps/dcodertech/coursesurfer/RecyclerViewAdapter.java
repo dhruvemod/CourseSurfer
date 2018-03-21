d6d96c4708e74812b4a0329a7a85b93d166e0537
Dhruve Moudgil <dhruve.moudgildm@gmail.com>
2018-03-21 10:28:16 +0530

    added listener to cardview

--- app/src/main/java/com/apps/dcodertech/coursesurfer/RecyclerViewAdapter.java
+++ app/src/main/java/com/apps/dcodertech/coursesurfer/RecyclerViewAdapter.java
@@ -1,12 +1,15 @@
 package com.apps.dcodertech.coursesurfer;
 
 import android.content.Context;
+import android.content.Intent;
+import android.support.v7.widget.CardView;
 import android.support.v7.widget.ListPopupWindow;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.List;
 
@@ -23,9 +26,11 @@ public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapte
         return courses.get(position);
     }
     public class MyViewHolder extends RecyclerView.ViewHolder{
+        private CardView cardView;
         public TextView title,author,company,provider,univ,certification,week,hours;
         public MyViewHolder(View view){
             super(view);
+            cardView=view.findViewById(R.id.cardView);
             title=view.findViewById(R.id.cardTitleView);
             author=view.findViewById(R.id.cardAuthorsView);
             company=view.findViewById(R.id.CardCourseCompanyView);
@@ -35,6 +40,8 @@ public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapte
             week=view.findViewById(R.id.cardweeksView);
             hours=view.findViewById(R.id.cardhoursView);
         }
+
+
     }
     public RecyclerViewAdapter(Context context, List<Courses> courses){
         this.context=context;
@@ -47,13 +54,13 @@ public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapte
                 .inflate(R.layout.card_list_view, parent, false);
         final MyViewHolder holder = new MyViewHolder(itemView);
 
-        
+
 
         return new MyViewHolder(itemView);
     }
     @Override
     public void onBindViewHolder(final MyViewHolder holder, int position) {
-        Courses c = courses.get(position);
+        final Courses c = courses.get(position);
         holder.title.setText(c.getCourse_name());
         holder.author.setText(c.getCourse_prof());
         holder.company.setText(c.getCourse_subject());
@@ -62,7 +69,15 @@ public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapte
         holder.certification.setText(c.getCourse_certifications());
         holder.week.setText(c.getCourse_duration());
         holder.hours.setText(c.getCourse_hours());
-
+        holder.cardView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String url = c.getCourse_link();
+                Intent intent=new Intent(view.getContext(),webView.class);
+                intent.putExtra("webLink",url);
+                context.startActivity(intent);
+            }
+        });
           }
     @Override
     public int getItemCount() {
